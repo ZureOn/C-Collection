132aefa72ad4a428efa5b650bab037c917424ddb
Zanzara <38494639+ZureOn@users.noreply.github.com>
2018-04-19 20:52:26 +0300

    Vue examples
    added a sample vue example to /chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Basic ChatApp</title>
+		<link rel="stylesheet" href="css/app.css">
+	</head>
+	<body>
+		<h1>ChatRoom</h1>
+		<div id="app">
+			<example-component></example-component>
+		</div>		
+		<script src="js/app.js" charset="UTF-8"></script>
+	</body>
+</html>
